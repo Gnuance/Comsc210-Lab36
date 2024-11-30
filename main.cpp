a0fe3d5ffc46ed877cf601038a7e7c81b24888a8
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-11-30 15:53:14 -0800

    10 minute timestamp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@
 using namespace std;
 using namespace std::chrono; // So chrono:: doesn't have to be used over and over again
 
+int main_menu();                      // outputs prompt and collects user selection
+bool isValidOption(string, int, int); // helper function to validate user input
+
 int main()
 {
     // File to read from
@@ -43,7 +46,93 @@ int main()
         inputFile.close();
     }
 
-    cout << "Total lines: " << lineCount << "\nTotal time: " << elapsed.count() << " ms" << endl;
+    // prompt user for selection and input
+    // user selection 5 is the program exit code
+    int userSelectedOption;
+    do
+    {
+        userSelectedOption = main_menu();
+        switch (userSelectedOption)
+        {
+        case 1:
+            // Prints adjacency list representation of graph
+            graph.printGraph();
+            break;
+        case 2:
+            // Breadth first search from node 0
+            graph.BFS(startingNode);
+            break;
+        case 3:
+            // Depth first search from node 0
+            graph.DFS(startingNode);
+            break;
+        case 4:
+            // STEP 4 -- Shortest paths through graph from node 0
+            graph.printShortestPathsWithDetails(startingNode);
+            break;
+        case 5:
+            // STEP 5 -- MST using Prim's Algorithm
+            graph.primMST(startingNode);
+            break;
+        default:
+            break;
+        }
+    } while (userSelectedOption != 0);
+
+    cout << "Thank you for using the BST management tool" << endl;
+
+    // cout << "Total lines: " << lineCount << "\nTotal time: " << elapsed.count() << " ms" << endl;
 
     return 0;
+}
+
+// main menu fuction for STEP 6
+int main_menu()
+{
+    string userInput = "";
+
+    do
+    {
+        // output prompt
+        cout << "\nVPN: Network Menu" << "\n"
+             << "[1] Display Bandwidth Summary" << "\n"
+             << "[2] Default Proxy Map [BFS]" << "\n"
+             << "[3] Test Network Nodes [DFS]" << "\n"
+             << "[4] Display Minimum Bandwidth From Source" << "\n"
+             << "[5] MST: Minimum Spanning Tree For Network" << "\n"
+             << "[0] Quit" << "\n"
+             << "Choice --> ";
+        getline(cin, userInput); // get user input as string and test
+        cout << endl;
+    } while (!isValidOption(userInput, 0, 5));
+
+    // if isValidOption passed, stoi(userInput) has already been tested and is safe
+    return stoi(userInput);
+}
+
+// return t/f if userInput is a valid int between min and max
+// helper function for main_menu and delete_goat
+// WARNING: stoi() will convert a double to an int or any string following an int.
+// Ex: stoi("2.9") will return 2 and so will stoi("2tGznso"), etc.
+bool isValidOption(string userInput, int minOption, int maxOption)
+{
+    int selectedOption = 0;
+    try
+    {
+        selectedOption = stoi(userInput);
+    }
+    catch (const std::exception &e)
+    {
+        cout << "Invalid input: Please enter a valid integer." << "\n\n";
+        return false;
+    }
+
+    // if userInput is an int but outside expected range
+    if (selectedOption < minOption || selectedOption > maxOption)
+    {
+        cout << "Invalid input: Please enter an integer between " << minOption << " and " << maxOption << "." << "\n\n";
+        return false;
+    }
+
+    return true;
 }
